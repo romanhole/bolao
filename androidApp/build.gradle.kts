af83459d51fc65e95e19ae1d7b182b790bea2e4d
vntrano <rafael.borrozino@venturus.org.br>
2026-06-11 14:12:21 -0300

    chore(release): bump para v1.1.0 (versionCode 2)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.bolao.android"
         minSdk        = libs.versions.android.minSdk.get().toInt()
         targetSdk     = libs.versions.android.targetSdk.get().toInt()
-        versionCode   = 1
-        versionName   = "1.0.0"
+        versionCode   = 2
+        versionName   = "1.1.0"
     }
 
     signingConfigs {
