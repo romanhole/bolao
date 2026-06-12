c682ca0aa21ba9e4d05c9f9d6c181e864362dace
vntrano <rafael.borrozino@venturus.org.br>
2026-06-11 22:44:29 -0300

    Fix Vercel Preview Supabase API key payload mismatch

--- shared/src/wasmJsMain/kotlin/com/bolao/config/AppConfig.kt
+++ shared/src/wasmJsMain/kotlin/com/bolao/config/AppConfig.kt
@@ -16,7 +16,7 @@ actual object AppConfig {
 
     actual val SUPABASE_ANON_KEY: String
         get() = if (isDevEnvironment()) {
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlY3Z0aWRsamt3eWRtb3Bic2dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMTg5OTUsImV4cCI6MjA5NTAzOTIyM30.0k6bELat4llEYjcxuSP51K1F1UNb5rq4KQuzycOajbs"
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlY3Z0aWRsamt3eWRtb3Bic2dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMTg5OTUsImV4cCI6MjA5NDc5NDk5NX0.0k6bELat4llEYjcxuSP51K1F1UNb5rq4KQuzycOajbs"
         } else {
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVldGRvbm5veXRic2p2dWxpaWVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NjMyMzIsImV4cCI6MjA5NTAzOTIzMn0.aBeDb9mUJlrtH0VNip8lSTpJ7hVpthhL3Nj2IhMOGRk"
         }
