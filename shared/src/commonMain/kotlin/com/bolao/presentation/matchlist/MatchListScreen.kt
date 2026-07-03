5bb27b84d7994c92e3891b589bc7842beecf8e6e
Romanhole <rafa.rborroz@gmail.com>
2026-07-03 11:28:39 -0300

    fix: ajusta regras do detekt no import

--- shared/src/commonMain/kotlin/com/bolao/presentation/matchlist/MatchListScreen.kt
+++ shared/src/commonMain/kotlin/com/bolao/presentation/matchlist/MatchListScreen.kt
@@ -18,10 +18,12 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -31,8 +33,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import org.koin.compose.viewmodel.koinViewModel
 
 /**
